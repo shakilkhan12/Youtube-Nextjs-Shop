53cc6f7688f9f902a7d08a4a64eb0c71da2866f5
shakilkhan12 <shakilkhan621@gmail.com>
2023-03-25 13:06:32 +0500

    displayed products details

--- src/pages/product/[id].js
+++ src/pages/product/[id].js
@@ -1,7 +1,40 @@
+import Rating from "@/components/Rating";
+import Head from "next/head";
+import Image from "next/image";
 import { useRouter } from "next/router";
 
 const Details = ({ product }) => {
-  return <h1>details</h1>;
+  const { title, image, category, rating, price, description } = product;
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <div className="max-w-screen-xl mx-auto px-5 my-20">
+        <div className="grid w-full md:grid-cols-2 gap-10">
+          <div className="border px-5 py-20 rounded-lg">
+            <div className="relative w-full h-[300px]">
+              <Image
+                src={image}
+                fill
+                className="w-full h-full object-contain"
+                alt={title}
+              />
+            </div>
+          </div>
+          <div>
+            <span className="uppercase text-gray-400 text-sm font-normal block mb-2">
+              {category}
+            </span>
+            <h2 className="text-[20px] font-medium mb-3 mt-2">{title}</h2>
+            <Rating rating={rating} />
+            <h2 className="text-2xl font-semibold mt-3">${price}</h2>
+            <p className="mt-6 text-gray-500 leading-[22px]">{description}</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 export default Details;
 export async function getServerSideProps(context) {
